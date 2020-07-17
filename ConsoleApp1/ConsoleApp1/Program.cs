db059d590113aac365ee75fbccaa32013922bc39
Khanh Trần <40160447+KhanhTran0112@users.noreply.github.com>
2020-07-17 19:43:01 +0700

    .

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Excel = Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.UI;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        public string sql = @"Data Source = DESKTOP-8GEUI6U; Initial Catalog = PrintfulTshirt; Integrated Security = True";
+        public static string sql = @"Data Source = DESKTOP-8GEUI6U; Initial Catalog = GameAndApp; Integrated Security = True";
+
+        public static List<GameAndApp> apps = new List<GameAndApp>();
+
         public static void ReadExcel()
         {
             Excel.Application xlApp = new Excel.Application();
@@ -22,8 +29,6 @@ namespace ConsoleApp1
             int rowCount = xlRange.Rows.Count;
             int colCount = xlRange.Columns.Count;
 
-            //iterate over the rows and columns and print to the console as it appears in the file
-            //excel is not zero based!!
             for (int i = 2; i <= rowCount; i++)
             {
                 string name = "null";
@@ -47,7 +52,7 @@ namespace ConsoleApp1
 
                 List<GameAndApp> gameAndApps = new List<GameAndApp>();
                 gameAndApps.Add(new GameAndApp(name, urlApp, ageApp, pricing, size, category, description, developer));
-
+                AddToDatabase(gameAndApps, i);
             }
 
             GC.Collect();
@@ -63,16 +68,91 @@ namespace ConsoleApp1
             Marshal.ReleaseComObject(xlApp);
         }
 
-        public static void AddToDatabase(List<GameAndApp> games)
+        public static void AddToDatabase(List<GameAndApp> games, int i)
         {
+            SqlConnection connection = new SqlConnection(sql);
+            connection.Open();
+            string cmdText = "INSERT INTO GAMEIOS_TABLE(Name, URLApp, AGEApp, Pricing, Size, Category, Description, Developer) " +
+                "VALUES ('" + games[0].name + "', '" + games[0].urlApp + "', '" + games[0].ageApp + "', '" + games[0].pricing + "', '" + games[0].size + "', '" + games[0].category + "', '" + games[0].description + "', '" + games[0].developer + "')";
+            SqlCommand sqlCommand = new SqlCommand(cmdText, connection);
+            sqlCommand.ExecuteNonQuery();
+            connection.Close();
+            Console.WriteLine("Đã xong " + (i - 1));
 
+        }
+
+        public static void ReadData()
+        {
+            SqlConnection connection = new SqlConnection(sql);
+            connection.Open();
+            SqlCommand sqlCommand = connection.CreateCommand();
+            sqlCommand.CommandText = "Select * from GAMEIOS_TABLE";
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                string description = "z" + reader[7].ToString() + "z";
+                description = description.Replace("zDescription", "").Replace("morez", "");
+                apps.Add(new GameAndApp(reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString(), reader[5].ToString(), reader[6].ToString(), description, reader[8].ToString() ));
+                //Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString());
+            }
         }
 
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            ReadExcel();
+            //ReadExcel();
+            ReadData();
 
+            Console.Write("Gõ run để chạy: ");
+            string run = Console.ReadLine();
+            if (run == "run")
+            {
+                ChromeDriver chromeDriver = new ChromeDriver();
+                chromeDriver.Url = "https://ahihisoftware.com/login";
+                chromeDriver.Navigate();
+
+                var email = chromeDriver.FindElementByXPath("/html/body/div/div[2]/form/div[1]/input");
+                email.SendKeys("nguyencuonggostack@gmail.com");
+
+                var password = chromeDriver.FindElementByXPath("/html/body/div/div[2]/form/div[2]/input");
+                password.SendKeys("Toang123!@#");
+
+                var login = chromeDriver.FindElementByXPath("/html/body/div/div[2]/form/div[3]/div[2]/button");
+                login.Click();
+
+                var createApp = chromeDriver.FindElementByXPath("/html/body/div/aside/section/ul/li[2]/ul/li[2]/a");
+                createApp.Click();
+
+                for (int i = 0; i < apps.Count; i++)
+                {
+                    var title = chromeDriver.FindElementByXPath("/html/body/div/div/section[2]/div/div/div/form/div[1]/div[1]/input");
+                    title.SendKeys(apps[1].name);
+
+                    var description = chromeDriver.FindElementByXPath("/html/body/div/div/section[2]/div/div/div/form/div[1]/div[2]/input");
+                    description.SendKeys(apps[1].name);
+
+                    SelectElement select = new SelectElement(chromeDriver.FindElementByXPath("/html/body/div/div/section[2]/div/div/div/form/div[1]/div[3]/div[1]/div/div/button/div/div"));
+                    select.SelectByText("Option3");
+                    select.SelectByValue("");
+
+
+                    var developer = chromeDriver.FindElementByXPath("/html/body/div/div/section[2]/div/div/div/form/div[1]/div[4]/div[1]/div/input");
+                    developer.SendKeys(apps[1].developer);
+
+                    var urlApp = chromeDriver.FindElementByXPath("/html/body/div/div/section[2]/div/div/div/form/div[1]/div[4]/div[2]/div/input");
+                    urlApp.SendKeys(apps[1].urlApp);
+
+                    var fileSize = chromeDriver.FindElementByXPath("/html/body/div/div/section[2]/div/div/div/form/div[1]/div[5]/div[2]/div/input");
+                    fileSize.SendKeys(apps[1].size);
+
+                    var fullDescription = chromeDriver.FindElementByClassName("wysihtml5-sandbox");
+                    
+                    fullDescription.SendKeys(apps[1].description.Trim());
+                    Console.WriteLine("asdasd");
+                }
+
+
+            }
             Console.ReadKey();
         }
     }
